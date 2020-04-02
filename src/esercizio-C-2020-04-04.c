360bfd734b7734493e98e39e5bfd8a74e008966d
utente <utente@debian10.units.it>
2020-04-02 23:39:19 +0200

    Fix

--- src/esercizio-C-2020-04-04.c
+++ src/esercizio-C-2020-04-04.c
@@ -1,15 +1,4 @@
-/**
- * scrivere un programma che legge da stdin una riga alla volta (la riga si considera terminata dal carattere \n).
-
-la lunghezza della riga è arbitraria, non fare ipotesi sulla massima lunghezza di riga.
-[in questo esercizio, non occorre immagazzinare l'intera riga].
-
-calcolare la media della lunghezza (=numero di caratteri) delle righe lette che vengono lette da stdin.
-
-dopo ogni riga letta, mostrare la lunghezza della riga e la media "parziale" fino a quel momento.
-
-il programma termina in caso di EOF su stdin.
- */
+/** ex 2020-04-04 */
 #include <stdio.h>
 #include <string.h>
 
