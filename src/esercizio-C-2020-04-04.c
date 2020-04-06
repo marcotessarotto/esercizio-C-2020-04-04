0723bc411c0cc767956e822b8bf9ec80a3adff01
pierbin24 <63203868+pierbin24@users.noreply.github.com>
2020-04-06 11:36:21 +0200

    Update esercizio-C-2020-04-04.c

--- src/esercizio-C-2020-04-04.c
+++ src/esercizio-C-2020-04-04.c
@@ -1,1 +1,37 @@
-//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+
+	char c;
+	float n_righe = 0;
+	int len_riga = 0;
+	float cTot = 0;
+	float media;
+
+
+	while((c=getchar()) != EOF){
+
+		if (c == '\n'){
+			printf("La lunghezza della riga seguente è pari a: %d \n", len_riga);
+			n_righe++;
+			media = (cTot/n_righe);
+			printf("La media per ogni riga fino ad ora è pari a: %.2f \n", media);
+			//imposto a -1 e non ha zero per non contare \n
+			len_riga =-1;
+			// per lo stesso motivo decremento cTot
+			cTot --;
+			printf("_____________________________________\n\n");
+
+		}
+
+		cTot++;
+		len_riga++;
+
+
+	}
+
+}
