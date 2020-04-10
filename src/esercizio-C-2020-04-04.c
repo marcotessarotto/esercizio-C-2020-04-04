f1ea3503a057af01d36ccc1177af574430112a33
Spopoi <61315723+Spopoi@users.noreply.github.com>
2020-04-10 15:32:51 +0200

    Update esercizio-C-2020-04-04.c
    final commit

--- src/esercizio-C-2020-04-04.c
+++ src/esercizio-C-2020-04-04.c
@@ -1,58 +1,26 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define NUM_OF_CHAR 30
-double averageLength;
-
-int lenghtOfArray(char *charArray, int arrayLength) {
-	int counter = 0;
-	for (int i = 0; i < arrayLength; i++) {
-		if (charArray[i] != 0) {
-			counter++;
-		}
-	}
-	return counter;
-}
 
 int main(int argc, char *argv[]) {
 	char inputChar;
-	char *currentLine = calloc(NUM_OF_CHAR, sizeof(char));
+	int numberOfLine = 1;
 	int index = 0;
-	int indexLength = 1; //used for count how many time realloc was invoked in order to give back the correct memory spaces
-	int numOfLine = 0;
-	printf("inserisci linee di caratteri separate dal carattere spazio \n");
-
+	double average;
+	printf("inserisci le stringhe separate dal carattere spazio:");
 	while ((inputChar = getchar()) != EOF) {
-		if (index == (NUM_OF_CHAR * indexLength)) {
-			indexLength++;
-			if ((realloc(currentLine, NUM_OF_CHAR * indexLength)) == NULL) {
-				perror("realloc error!");
-				exit(EXIT_FAILURE);
-			}
-		}
-		if (inputChar != '\n') {
-			currentLine[index] = inputChar;
-			index++;
-		} else {
-			numOfLine++;
-			int lengthOfCurrentLine = lenghtOfArray(currentLine,
-			NUM_OF_CHAR * indexLength);
-			if (numOfLine == 1) {
-				averageLength = lengthOfCurrentLine;
-			}
-			averageLength = (averageLength + lengthOfCurrentLine) / 2;
+		if (inputChar != '\n')
+			index++; //counting number of char
+		else { // new line
+			if (numberOfLine == 1) //first execution
+				average = index;
+			else
+				average = (average + index) / 2;
 			printf(
 					"la lunghezza della linea corrente è:%d \nla media parziale è: %.2f\n",
-					lengthOfCurrentLine, averageLength);
-			memset(currentLine, 0, sizeof(char) * (NUM_OF_CHAR * indexLength));
-			index = 0;
-			indexLength = 1;
-			if ((realloc(currentLine, NUM_OF_CHAR * indexLength)) == NULL) {
-				perror("realloc error!");
-				exit(EXIT_FAILURE);
-			}
+					index, average);
+			index = 0; //reset number of char
+			numberOfLine++;
 		}
 	}
-	free(currentLine);
+	printf("EOF raggiunto");
 	return 0;
 }
